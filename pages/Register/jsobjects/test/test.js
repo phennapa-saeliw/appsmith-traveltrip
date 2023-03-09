ac0384e1e07b1b2a73361608181a31a3f54546f7
kasama.cha <kasama.cha@mfec.co.th>
2023-03-09 09:57:07 +0000

    change button ui

---
+++ pages/Register/jsobjects/test/test.js
@@ -0,0 +1,14 @@
+export default {
+
+	myFun2: async () => {
+		
+	 let username = input_username.inputText
+	 let checkusername = 'kasama1'
+	 	if(check_username.data > 0){
+			showAlert('มีผู้ใช้งานบัญชีนี้อยู่เเล้ว','error')
+			
+		}
+		
+	
+	}
+}
