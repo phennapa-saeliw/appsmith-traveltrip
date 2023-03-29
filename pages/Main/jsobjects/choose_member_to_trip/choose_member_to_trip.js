6e18a7a99acb6e3194ff65c2ae7696f97fbe3d79
kasama.cha <kasama.cha@mfec.co.th>
2023-03-29 04:33:51 +0000

    เปลี่ยนวิธีเพิ่มสมาชิก

--- pages/Main/jsobjects/choose_member_to_trip/choose_member_to_trip.js
+++ pages/Main/jsobjects/choose_member_to_trip/choose_member_to_trip.js
@@ -5,20 +5,17 @@ export default {
 		select_from_detail.run()
 		let trip_id = select_from_detail.data[0].event_trip_id
 		
-		storeValue('trip_id',trip_id)
-	  
+		storeValue('trip_id',trip_id);
 		
-	  let user_select  = JSONForm1CopyCopy.formData.customField4;
-	
- 	  for(let i=0;i < user_select.length;i++){                         
-	 		const user_num = user_select[i];    
-		 //console.log(user_num);
-		 
-		 storeValue('user_id',user_num)
-
-		 await create_trip_user_JS.run()
- 
-	} 
-	
-}
+		let user_select = JSONForm1CopyCopy.formData.customfield4;
+		for( let i = 0; i < user_select.length; i++){
+			const user_num = user_select[i];
+			
+			storeValue('user_num',user_num)
+			
+			await create_trip_user_JS_2.run()
+		}
+		
+	 
+  }
 }
